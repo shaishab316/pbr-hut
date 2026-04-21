d5d41803d2e3b06af1b130101752ea9f199f2085
shaishab316 <shaishabchandrashil@gmail.com>
2026-04-21 13:31:35 +0600

    fix(app): Fix the warning of @Get(*)
    Unsupported route path: "/api/v1/*". In previous versions, the symbols ?, *, and + were used to denote optional or repeating path parameters. The latest version of "path-to-regexp" now requires the use of named parameters. For example, instead of using a route like /users/* to capture all routes starting with "/users", you should use /users/*path. For more details, refer to the migration guide. Attempting to auto-convert... {"context":"LegacyRouteConverter","level":"warn"}

--- src/app.module.ts
+++ src/app.module.ts
@@ -92,6 +92,6 @@ import LokiTransport from 'winston-loki';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*path');
   }
 }
