6695bf0a216d7d14d976fccca9bf7321a3fd8b04
shaishab316 <shaishabchandrashil@gmail.com>
2026-04-22 09:02:46 +0600

    feat(restaurant): add caching for primary restaurant retrieval and update

--- src/modules/restaurant/restaurant.controller.ts
+++ src/modules/restaurant/restaurant.controller.ts
@@ -3,12 +3,19 @@ import { UpdatePrimaryRestaurantDto } from './dto/update-primary.dto';
 import { RestaurantService } from './restaurant.service';
 import { JwtGuard, RolesGuard } from '@/common/guards';
 import { Roles } from '@/common/decorators';
+import {
+  CacheKey,
+  CacheTTL,
+  InvalidateCache,
+} from '@/common/decorators/cache.decorator';
 
 @Controller('restaurant')
 export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
 
   @Get('/primary')
+  @CacheKey('primary-restaurant')
+  @CacheTTL(3600) //? 1 hour
   async getPrimary() {
     const data = await this.restaurantService.getPrimary();
 
@@ -21,6 +28,7 @@ export class RestaurantController {
   @Post('/update-primary')
   @UseGuards(JwtGuard, RolesGuard)
   @Roles('ADMIN')
+  @InvalidateCache('primary-restaurant')
   async updatePrimary(@Body() dto: UpdatePrimaryRestaurantDto) {
     const data = await this.restaurantService.updatePrimary(dto);
 
